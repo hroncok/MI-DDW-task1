bf7b9d3595951a45208f4da4d0cd7d394d7f823b
Miro Hrončok <miro@hroncok.cz>
2015-03-26 16:22:00 +0100

    Basic form to get URL

--- webapp/gate/views.py
+++ webapp/gate/views.py
@@ -1,5 +1,15 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+import forms
 
 def index(request):
     '''The homepage'''
-    return render(request, 'index.html')
+    url = ''
+    if request.method == 'POST':
+        form = forms.URLForm(request.POST)
+        if form.is_valid():
+            url = form.cleaned_data['url']
+    else:
+        form = forms.URLForm()
+    return render(request, 'index.html', {'form': form, 'url': url})
