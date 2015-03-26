172f78941abcc8ebd8672acc63fa13222187eccb
Miro Hrončok <miro@hroncok.cz>
2015-03-26 21:53:18 +0100

    Display some output based on the Java CLI tool

---
+++ webapp/gate/logic.py
@@ -0,0 +1,46 @@
+import html2text
+import requests
+from subprocess import Popen, PIPE, STDOUT
+
+
+def text_from_url(url):
+    '''Returns string of text from the given URL'''
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            return None
+        h = html2text.HTML2Text()
+        h.ignore_links = True
+        h.ignore_images = True
+        h.ignore_emphasis = True
+        return h.handle(response.text)
+        
+    except requests.ConnectionError:
+        return None
+
+
+def gate(text):
+    '''Runs the given text trough the Java app'''
+    jarfile = '../GATE-counter/dist/GATE-counter.jar'
+    p = Popen(['java', '-jar', jarfile], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    return p.communicate(input=text.encode('UTF-8'))[0]
+    
+
+
+def tokenize(url):
+    '''Tokenizes the given URL'''
+    text = text_from_url(url)
+    major = {}
+    minor = {}
+    for line in gate(text).split('\n'):
+        if not line:
+            continue
+        key, value = line.strip().split()
+        if ':' not in key:
+            major[key] = value
+        else:
+            primary, secondary = key.split(':')
+            if not primary in minor:
+                minor[primary] = {}
+            minor[primary][secondary] = value
+    return major, minor
--- webapp/gate/views.py
+++ webapp/gate/views.py
@@ -2,14 +2,16 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 import forms
+import logic
 
 def index(request):
     '''The homepage'''
-    url = ''
+    major = minor = {}
     if request.method == 'POST':
         form = forms.URLForm(request.POST)
         if form.is_valid():
             url = form.cleaned_data['url']
+            major, minor = logic.tokenize(url)
     else:
         form = forms.URLForm()
-    return render(request, 'index.html', {'form': form, 'url': url})
+    return render(request, 'index.html', {'form': form, 'major': major, 'minor': minor})
